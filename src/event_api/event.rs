29956eea16724b0eeecf2349ee2f39ee783b9352
Evan Reeves <web@evanreeves.com>
2022-10-15 20:59:25 +0200

    feat: restructure Message enum variant to support multiple subtypes
    Removes some of the duplicative enum definitions which only served to
    return the "block_type". Create a new enum which will contain all of the
    message subtypes available in the Events API

--- src/event_api/event.rs
+++ src/event_api/event.rs
@@ -16,94 +16,6 @@ pub enum Event {
     EventCallback(EventCallback),
 }
 
-impl Event {
-    pub fn block_type(&self) -> EventType {
-        match self {
-            Event::EventCallback(event_callback) => match event_callback.event {
-                EventCallbackType::AppHomeOpened { .. } => EventType::AppHomeOpened,
-                EventCallbackType::AppMention { .. } => EventType::AppMention,
-                EventCallbackType::AppRateLimited { .. } => EventType::AppRateLimited,
-                EventCallbackType::AppRequested { .. } => EventType::AppRequested,
-                EventCallbackType::AppUninstalled { .. } => EventType::AppUninstalled,
-                EventCallbackType::ChannelArchive { .. } => EventType::ChannelArchive,
-                EventCallbackType::ChannelCreated { .. } => EventType::ChannelCreated,
-                EventCallbackType::ChannelDeleted { .. } => EventType::ChannelDeleted,
-                EventCallbackType::ChannelHistoryChanged { .. } => EventType::ChannelHistoryChanged,
-                EventCallbackType::ChannelIDChanged { .. } => EventType::ChannelIDChanged,
-                EventCallbackType::ChannelLeft { .. } => EventType::ChannelLeft,
-                EventCallbackType::ChannelRename { .. } => EventType::ChannelRename,
-                EventCallbackType::ChannelShared { .. } => EventType::ChannelShared,
-                EventCallbackType::ChannelUnarchive { .. } => EventType::ChannelUnarchive,
-                EventCallbackType::ChannelUnshared { .. } => EventType::ChannelUnshared,
-                EventCallbackType::EmojiChanged { .. } => EventType::EmojiChanged,
-                EventCallbackType::GridMigrationFinished { .. } => EventType::GridMigrationFinished,
-                EventCallbackType::GridMigrationStarted { .. } => EventType::GridMigrationStarted,
-                EventCallbackType::GroupArchive { .. } => EventType::GroupArchive,
-                EventCallbackType::GroupClose { .. } => EventType::GroupClose,
-                EventCallbackType::GroupDeleted { .. } => EventType::GroupDeleted,
-                EventCallbackType::GroupHistoryChanged { .. } => EventType::GroupHistoryChanged,
-                EventCallbackType::GroupLeft { .. } => EventType::GroupLeft,
-                EventCallbackType::GroupOpen { .. } => EventType::GroupOpen,
-                EventCallbackType::GroupRename { .. } => EventType::GroupRename,
-                EventCallbackType::GroupUnarchive { .. } => EventType::GroupUnarchive,
-                EventCallbackType::ImClose { .. } => EventType::ImClose,
-                EventCallbackType::ImCreated { .. } => EventType::ImCreated,
-                EventCallbackType::ImHistoryChanged { .. } => EventType::ImHistoryChanged,
-                EventCallbackType::ImOpen { .. } => EventType::ImOpen,
-                EventCallbackType::InviteRequested { .. } => EventType::InviteRequested,
-                EventCallbackType::LinkShared => EventType::LinkShared,
-                EventCallbackType::MemberJoinedChannel { .. } => EventType::MemberJoinedChannel,
-                EventCallbackType::MemberLeftChannel { .. } => EventType::MemberLeftChannel,
-                EventCallbackType::Message { .. } => EventType::Message,
-                EventCallbackType::Other => EventType::Other,
-            },
-        }
-    }
-}
-
-/// [Event API Type](https://api.slack.com/events?filter=Events)
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub enum EventType {
-    AppHomeOpened,
-    AppMention,
-    AppRateLimited,
-    AppRequested,
-    AppUninstalled,
-    ChannelArchive,
-    ChannelCreated,
-    ChannelDeleted,
-    ChannelHistoryChanged,
-    ChannelIDChanged,
-    ChannelLeft,
-    ChannelRename,
-    ChannelShared,
-    ChannelUnarchive,
-    ChannelUnshared,
-    EmojiChanged,
-    GridMigrationFinished,
-    GridMigrationStarted,
-    GroupArchive,
-    GroupClose,
-    GroupDeleted,
-    GroupHistoryChanged,
-    GroupLeft,
-    GroupOpen,
-    GroupRename,
-    GroupUnarchive,
-    ImClose,
-    ImCreated,
-    ImHistoryChanged,
-    ImOpen,
-    InviteRequested,
-    LinkShared,
-    MemberJoinedChannel,
-    MemberLeftChannel,
-    Message,
-    #[serde(other)]
-    Other,
-}
-
 #[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct EventCallback {
@@ -288,33 +200,45 @@ pub enum EventCallbackType {
         team: String,
     },
     /// A message was sent to a channel. Note that this can include a variety of subtypes
-    Message {
-        channel_type: String,
-        channel: String,
-        event_ts: String,
-        hidden: Option<bool>,
-        subtype: Option<MessageSubtype>,
-        text: String,
-        thread_ts: Option<String>,
-        ts: String,
-        user: Option<String>,
-    },
+    #[serde(rename = "message")]
+    Message(MessageSubtype),
     #[serde(other)]
     Other,
 }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
-#[skip_serializing_none]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "snake_case", tag = "subtype")]
 pub enum MessageSubtype {
-    BotMessage,
+    BotMessage {
+        ts: String,
+        text: String,
+        bot_id: String,
+        username: String,
+    },
     MeMessage,
     MessageChanged {
-        _type: String,
         user: String,
         text: String,
         ts: String,
-    }
+        edited: MessageChangedEdited,
+    },
+    Message {
+        channel_type: String,
+        channel: String,
+        event_ts: String,
+        hidden: Option<bool>,
+        text: String,
+        thread_ts: Option<String>,
+        ts: String,
+        user: Option<String>,
+    },
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct MessageChangedEdited {
+    user: String,
+    ts: String,
 }
 
 #[cfg(test)]
@@ -384,7 +308,7 @@ mod test {
     "event_id": "Ev0464MAU18R",
     "event_time": 1665341482
 }"##;
-        let event = serde_json::from_str::<EventCallback>(json);
+        let event = serde_json::from_str::<Event>(json);
         assert_eq!(event.is_ok(), true);
     }
 
